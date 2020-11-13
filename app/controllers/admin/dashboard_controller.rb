954143990e206fa7ef37e4b099163c57cf8bbe13
csjohn1 <csjohn@shaw.ca>
2020-11-13 00:49:31 +0000

    adding dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,12 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['USER_NAME'], :password => ENV['PASSWORD']
 
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
+# def show 
+#   @unique_inventory = Product.count() 
+#   @category_count = Category.count
+# end
+# end
